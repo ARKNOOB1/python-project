baa0143ed20e916dbcb94c72ec33457ed022503e
ARKNOOB1 <134386543+ARKNOOB1@users.noreply.github.com>
2023-08-30 09:43:50 +0900

    1520 피금황 - 2023-08-30 파이썬2.py

--- 2023-08-30 파이썬2.py
+++ 2023-08-30 파이썬2.py
@@ -1,6 +1,6 @@
 h = float(input ("키 입력 (단위: m): ")) #키 입력하는 부분
 w = int(input ("몸무게 입력 (단위: kg): ")) #몸무게 입력하는 부분
 
-b = w/(h**2)
+b = w/(h**2) #BMI 구하는 부분
 
-print("체질량(BMI)이 %d입니다." %b)
+print("체질량(BMI)이 %d입니다." %b) #BMI 출력
